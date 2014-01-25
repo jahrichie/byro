2bcbc4b814bb269588ff91d658fe7bcf063057d5
jahrichie <roreilly@charitybuzz.com>
2014-01-24 22:09:01 -0500

    generated app by rails new cml

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Byro::Application.config.secret_token = '6e82bc356539b5b2c2c74686202de58bbfadb400e893a6c8e59949493e39f3fb8418b9dbd97b366217c9361afec61a892f4395e96843d05fef597d3e7fec838d'
