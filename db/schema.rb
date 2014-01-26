2dde4bb86fbadee6672c9477884484658c52b98e
jahrichie <roreilly@charitybuzz.com>
2014-01-25 19:34:21 -0500

    added handle to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140126002137) do
+ActiveRecord::Schema.define(:version => 20140126003403) do
 
   create_table "user_messages", :force => true do |t|
     t.integer  "from_user_id"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(:version => 20140126002137) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "handle"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
