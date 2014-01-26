c0c1de0afe42ab0df8b5bece88b4edc92b20c1c0
jahrichie <roreilly@charitybuzz.com>
2014-01-25 21:52:33 -0500

    has_many relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,12 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me,  :handle
   # attr_accessible :title, :body
+
+  has_many :user_messages, 
+    :foreign_key => :to_user_id
+
+
+  # scope :sent_messages, :conditions => { :highlight => true } #, :include => [:item]
+
+
 end
